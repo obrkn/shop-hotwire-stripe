79367eb1e3d728e403c4496fcebec448a7d0368b
obrkn <k33mdukb9n@yahoo.co.jp>
2023-10-11 13:50:03 -0700

    rubocop auto correct

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class CartItemsController < ApplicationController
   def index
     @cart_items = current_user.cart_items.eager_load(:product)
-    @total = @cart_items.map(&:product).map(&:price).sum
+    @total = @cart_items.map(&:product).sum(&:price)
     @cart_products = @cart_items.map(&:product).uniq
     @product_count_by_id = @cart_items.map(&:product_id).each_with_object(Hash.new(0)) { |id, counts| counts[id] += 1 }
   end
@@ -9,12 +11,12 @@ class CartItemsController < ApplicationController
   def create
     CartItem.create(product_id: params[:product_id], user_id: current_user.id)
     @cart_items = current_user.cart_items.eager_load(:product)
-    @total = @cart_items.map(&:product).map(&:price).sum
+    @total = @cart_items.map(&:product).sum(&:price)
   end
 
   def destroy
     CartItem.where(product_id: params[:id], user_id: current_user.id).destroy_all
     @cart_items = current_user.cart_items.eager_load(:product)
-    @total = @cart_items.map(&:product).map(&:price).sum
+    @total = @cart_items.map(&:product).sum(&:price)
   end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
     @products = Product.all
     @cart_items = current_user&.cart_items&.eager_load(:product) || []
-    @total = @cart_items.map(&:product).map(&:price).sum
+    @total = @cart_items.map(&:product).sum(&:price)
   end
 end
