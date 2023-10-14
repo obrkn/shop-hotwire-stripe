a6a93fb6aaac143d34613725143ed441b579a8e8
obrkn <k33mdukb9n@yahoo.co.jp>
2023-10-14 13:53:49 -0700

    change users session files

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :validatable
   has_many :cart_items, dependent: :destroy
 end
